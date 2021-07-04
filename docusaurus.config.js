920b70bbb17117e3867fe10be7e2cc2966d988a5
Harvey <harvey0379@163.com>
2021-07-04 15:14:06 +0800

    🔧 url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,20 +1,22 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const githubURL = 'https://github.com/Huauauaa/graduate-english';
+
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: '考研英语词汇',
+  tagline: '词根+联想记忆',
+  url: githubURL,
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Huauauaa',
+  projectName: 'graduate-english',
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: '考研英语词汇',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -28,7 +30,7 @@ module.exports = {
         },
         { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: githubURL,
           label: 'GitHub',
           position: 'right',
         },
@@ -72,12 +74,12 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: githubURL,
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Huauauaa, Inc. `,
     },
     prism: {
       theme: lightCodeTheme,
@@ -90,15 +92,11 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          editUrl: `${githubURL}/blob/master/`,
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          editUrl: `${githubURL}/blob/master/website/blog/`,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
